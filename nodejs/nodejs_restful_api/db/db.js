88e41577d3102f9448dbd29fcf0376e6c67153e6
Tiago Rodrigues Cunha <tiago.r.c.sn@gmail.com>
2020-02-11 18:09:49 -0300

    remove old project

--- nodejs/nodejs_restful_api/db/db.js
+++
@@ -1,9 +0,0 @@
-const test = [
-  {
-    id: 1,
-    title: "foo",
-    description: "This is some foo test to db"
-  }
-];
-
-export default test;
--- nodejs/nodejs_restful_api/utils/error.js
+++
@@ -1,6 +0,0 @@
-export default function send(err, req, res, code = 400) {
-  console.log(`error: ${err}`);
-  res.status(code).json({
-    error: err
-  });
-}
