c9c06d27bc9d7566fd6a728e529e5b4bd8886d05
Tiago Rodrigues Cunha <tiago.r.c.sn@gmail.com>
2026-06-23 00:10:36 -0300

    config redis server connection

--- Database/redis/src/server.ts
+++ Database/redis/src/server.ts
@@ -1,18 +1,12 @@
 import express from 'express';
 import routes from './routes.ts';
+import client from './client.ts';
 
 const app = express();
 
 app.use(routes);
+client.set('my_key', 'Hello, Redis!');
 
 app.listen(3333, () => {
   console.log("Server running on localhost:3333");
 });
-
-// import { createClient } from 'redis';
-
-// const client = createClient();
-
-// client.on('error', err => console.log('Redis Client Error', err));
-
-// await client.connect();
